f161a9efd9732099cca1c3b5850f58da39603f7a
Prajval Aggaral <125868043+Prajval-Aggarwal@users.noreply.github.com>
2023-03-30 11:59:26 +0530

    Update constants.go

--- Utils/constants.go
+++ Utils/constants.go
@@ -14,6 +14,3 @@ const Dbname = "gym_1"
 
 var SecretKey = []byte("My_Key")
 
-const TWILIO_ACCOUNT_SID string = "AC5869424c6ae2d0b27f66a5d5b9b90485"
-const TWILIO_AUTH_TOKEN string = "465b65f74039bb124fd93d990723c04c"
-const VERIFY_SERVICE_SID string = "VA6602f8535f8f1369b0ed68eed5d6af67"
